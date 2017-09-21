76dbfbffff1c9f295e456c7cc826cb796ad842e5
Easak Hong <easakhong@gmail.com>
2017-09-21 14:40:38 -0700

    Refactor slider to limit max to stack size

--- src/components/ActionBar.jsx
+++ src/components/ActionBar.jsx
@@ -20,7 +20,7 @@ class ActionBar extends React.Component {
 
     return (
       <div>
-        <Slider id="range" value={this.state.slider} onChange={this.handleSlider.bind(this)} min={0} max={this.props.sliderMax} step={1} />
+        <Slider id="range" value={this.state.slider} onChange={this.handleSlider.bind(this)} min={0} max={this.props.max} step={1} />
         <div disabled="true" id="call_button" onClick={() => this.props.bet(false)}>CHECK<br/>()</div>
         <div id="bet_button" onClick={() => this.props.bet(true)}>BET<br/>()</div>
         <div id="raise_button" onClick={this.props.allIn}>ALL IN<br/> </div>
